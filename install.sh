e116e1239c0708314a13fbf1f09e8eec760dfbe8
Chickcahbob <andrew.birn@gmail.com>
2026-01-04 17:43:10 -0600

    Add '-y' flag to apt-get install command

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get install seclists feroxbuster crowbar crackmapexec rubeus
+sudo apt-get install seclists feroxbuster crowbar crackmapexec rubeus -y
 sudo gunzip /usr/share/wordlists/rockyou.txt.gz
