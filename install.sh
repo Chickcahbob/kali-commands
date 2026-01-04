1faa6f63d6d3486b10b95e64748441ce25480d55
Chickcahbob <andrew.birn@gmail.com>
2026-01-04 17:43:01 -0600

    Add 'rubeus' to installation script

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get install seclists feroxbuster crowbar crackmapexec
+sudo apt-get install seclists feroxbuster crowbar crackmapexec rubeus
 sudo gunzip /usr/share/wordlists/rockyou.txt.gz
