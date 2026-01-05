ec4ea0f44e9d15818dbf9fea75c215137dbb6b6b
Chickcahbob <andrew.birn@gmail.com>
2026-01-04 18:29:11 -0600

    Update install.sh to include vim-plug installation
    Added command to install vim-plug for Neovim.

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo apt-get install seclists feroxbuster crowbar crackmapexec rubeus neovim -y
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo gunzip /usr/share/wordlists/rockyou.txt.gz
