1b545f7d05cf5f698fd703952c9f29cce94b097d
Chickcahbob <andrew.birn@gmail.com>
2026-01-04 18:32:23 -0600

    Update install.sh to include tmux and Neovim configs
    Added commands to download tmux configuration and Neovim init file.

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo apt-get install seclists feroxbuster crowbar crackmapexec rubeus neovim -y
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.tmux.conf --create-dirs \ https://raw.githubusercontent.com/Chickcahbob/configs/master/.tmux.conf
+curl -fLo ~/.config/nvim/init.vim --create-dirs \ https://raw.githubusercontent.com/Chickcahbob/configs/master/init.vim
 sudo gunzip /usr/share/wordlists/rockyou.txt.gz
