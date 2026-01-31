dce2f36a86c1bc4924545f0892b9e64fc75d95d0
Chickcahbob <andrew.birn@gmail.com>
2026-01-31 16:43:15 -0600

    Add 'chisel' to installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install seclists feroxbuster crowbar crackmapexec rubeus neovim -y
+sudo apt-get install seclists feroxbuster crowbar crackmapexec rubeus neovim chisel -y
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.tmux.conf --create-dirs \ https://raw.githubusercontent.com/Chickcahbob/configs/master/.tmux.conf
 curl -fLo ~/.config/nvim/init.vim --create-dirs \ https://raw.githubusercontent.com/Chickcahbob/configs/master/init.vim
